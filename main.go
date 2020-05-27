46e327811b807dd65692ce93ddde1f5dd4ef95f9
Jizishuo <jizishuo@qq.com>
2020-05-27 16:31:25 +0800

    config

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
-package Web_Api
+package main
+
+import (
+	"Web_Api/config"
+	"github.com/gin-gonic/gin"
+	"go-admin/models/gorm"
+	"log"
+	orm "Web_Api/database"
+)
+
+func main()  {
+	gin.SetMode(gin.DebugMode)
+	log.Println(config.DatabaseConfig.Port)
+
+	err := gorm.AutoMigrate(orm.Eloquent)
+	if err != nil {
+		log.Fatal("数据库基础数据初始化失败")
+	} else {
+		config.SetApplicationIsInit()
+	}
+
+	r := roou
+}
